f2e90eaed0c28bee879181036ff8c1612fadd13d
Petr Král <kralpet9@fjfi.cvut.cz>
2023-04-18 15:32:00 +0200

    fixed long row and perhaps the comment issue

--- code/quadratic.py
+++ code/quadratic.py
@@ -4,8 +4,10 @@ from math import sqrt
 # pylint: disable=invalid-name
 
 
+# explanatory comment
 def find_roots(a: int, b: int, c: int) -> Optional[tuple[float, float]]:
-    """This function should return a tuple (x1,x2) with the computed roots of a quadratic equation, given its coefficients a,b,c: ax^2 + bx + c = 0.
+    """This function should return a tuple (x1,x2) with the computed roots of a quadratic equation,
+    given its coefficients a,b,c: ax^2 + bx + c = 0.
     - Consider only real numbers as the domain. Return None otherwise.
     - The case where a = 0, should be prompted as invalid quadratic equation and return None.
     """
