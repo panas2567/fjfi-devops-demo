1f7cd90795ea5d7e53fbeadf0e63d510e80ef317
Petr Filip <petr.filip01@gmail.com>
2023-04-18 15:32:29 +0200

    case D < 0

--- code/quadratic.py
+++ code/quadratic.py
@@ -1,3 +1,6 @@
+"""
+- implementation of quadratic formula
+"""
 from typing import Optional
 
 import math
@@ -5,13 +8,16 @@ import math
 
 
 def find_roots(a: int, b: int, c: int) -> Optional[tuple[float, float]]:
-    """This function should return a tuple (x1,x2) with the computed roots of a quadratic equation, given its coefficients a,b,c: ax^2 + bx + c = 0.
+    """This function should return a tuple (x1,x2) with the computed roots of a quadratic equation, 
+    - given its coefficients a,b,c: ax^2 + bx + c = 0.
     - Consider only real numbers as the domain. Return None otherwise.
     - The case where a = 0, should be prompted as invalid quadratic equation and return None.
     """
     if(a == 0):
         return None
-    else:
-        x1 = (-b + math.sqrt(b*b - 4*a*c))/(2*a)
-        x2 = (-b - math.sqrt(b*b - 4*a*c))/(2*a)
+    D = b*b - 4*a*c
+    if(D>0):
+        x1 = (-b + math.sqrt(D))/(2*a)
+        x2 = (-b - math.sqrt(D))/(2*a)
         return (x1, x2)
+    return None
