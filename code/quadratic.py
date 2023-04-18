61bdac36fae55def5a00eeaf096798f67b9f0024
Petr Filip <petr.filip01@gmail.com>
2023-04-18 15:25:18 +0200

    case a = 0 done

--- code/quadratic.py
+++ code/quadratic.py
@@ -9,6 +9,9 @@ def find_roots(a: int, b: int, c: int) -> Optional[tuple[float, float]]:
     - Consider only real numbers as the domain. Return None otherwise.
     - The case where a = 0, should be prompted as invalid quadratic equation and return None.
     """
-    x1 = (-b + math.sqrt(b*b - 4*a*c))/(2*a)
-    x2 = (-b - math.sqrt(b*b - 4*a*c))/(2*a)
-    return (x1, x2)
+    if(a == 0):
+        return None
+    else:
+        x1 = (-b + math.sqrt(b*b - 4*a*c))/(2*a)
+        x2 = (-b - math.sqrt(b*b - 4*a*c))/(2*a)
+        return (x1, x2)
