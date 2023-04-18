1112d394d06e5e38db16cc4e395bd11b8990745b
Petr Král <kralpet9@fjfi.cvut.cz>
2023-04-18 15:15:23 +0200

    text

--- code/quadratic.py
+++ code/quadratic.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from math import sqrt
 # pylint: disable=invalid-name
 
 
@@ -8,4 +8,8 @@ def find_roots(a: int, b: int, c: int) -> Optional[tuple[float, float]]:
     - Consider only real numbers as the domain. Return None otherwise.
     - The case where a = 0, should be prompted as invalid quadratic equation and return None.
     """
-    return None
+    result = ()
+    D = b*b - 4*a*c
+    result[ 0 ] = ( -b + sqrt( D ) ) / ( 2 * a )
+    result[ 1 ] = ( -b - sqrt( D ) ) / ( 2 * a )
+    return result
