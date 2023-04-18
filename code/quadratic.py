243e95317a17da9cdb643c92d514166850c6f422
Kyrylo Herasymenko <heraskyr@fel.cvut.cz>
2023-04-18 15:32:14 +0200

    Added comment to begin, deleted long comment

--- code/quadratic.py
+++ code/quadratic.py
@@ -1,3 +1,4 @@
+# Added implementation
 from typing import Optional
 
 
@@ -5,13 +6,11 @@ from typing import Optional
 
 
 def find_roots(a: int, b: int, c: int) -> Optional[tuple[float, float]]:
-    """This function should return a tuple (x1,x2) with the computed roots of a quadratic equation, given its coefficients a,b,c: ax^2 + bx + c = 0.
-    - Consider only real numbers as the domain. Return None otherwise.
-    - The case where a = 0, should be prompted as invalid quadratic equation and return None.
-    """
-    if a == 0: return None
+    if a == 0:
+        return None
     d = b * b - 4 * a * c
-    if d < 0: return None
+    if d < 0:
+        return None
     x1 = (-1 * b - pow(d, 0.5)) / (2 * a)
     x2 = (-1 * b + pow(d, 0.5)) / (2 * a)
     return x1, x2
